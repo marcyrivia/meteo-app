5c17c813a60d5eab1579ded744505d1b9a685d14
zirael <goumdissshamsddine@gmailcom>
2023-11-03 19:21:28 +0100

    boucle for tempWeek

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,10 @@
 let city = "Bagdad";
+// let emoji = ""
+// if (description.includes("pluie")){
+//   emoji = ""
+// }
+// const weatherIconUrl = `https://openweathermap.org/img/wn/${item.weather[0].icon}.png`
+   // <img src="${weatherIconUrl}" alt="Weather Icon">)
 
 fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=da9cb24a6099240a11d9da9cebf23d28&lang=fr&units=metric`)
 
@@ -50,23 +56,30 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=da9cb24a
 
   console.log(`Population of ${data.city.name}: ${data.city.population}`);
 
-  const tempWeek= document.querySelector("#tempWeek");
+  const tempWeek = document.querySelector("#tempWeek");
 
-  for (const prevMeteo of data.list) {
-    console.log(prevMeteo.dt_txt);
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
 
-    const temperature = prevMeteo.main.temp;
-    const description = prevMeteo.weather[0].description;
+  data.list.forEach(prevMeteo => {
+    const prevDate = new Date(prevMeteo.dt * 1000); // Convertir la date UNIX en objet Date
 
-    const card = document.createElement("div");
-    card.classList.add("tempMain");
+    // Filtrer les prévisions à partir du lendemain
+    if (prevDate >= tomorrow) {
+      const temperature = prevMeteo.main.temp;
+      const description = prevMeteo.weather[0].description;
 
-    tempWeek.innerHTML = `
-      <p class="date"><b>${prevMeteo.dt_txt}</b></p>
-      <h2 class="temperature">${temperature}°C</h2>
-      <p class="description">${description}</p>
-    `;
+      const card = document.createElement("div");
+      card.classList.add("tempDay"); // Assurez-vous que la classe est correcte
 
-    tempWeek.appendChild(card);
-  }
+      card.innerHTML = `
+        <p class="dateW"><b>${prevDate.toLocaleDateString("fr-FR")}</b></p>
+        <h2 class="temperatureW">${temperature}°C</h2>
+        <p class="descriptionW">${description}</p>
+      `;
+
+      tempWeek.appendChild(card);
+    }
+  });
 });
